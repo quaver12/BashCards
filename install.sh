68fb2513dec8dd1009098a4bcad9fb8ccf54bc59
Marcus Branney <mbranney12@icloud.com>
2026-01-13 15:52:19 +0000

    install script slightly more verbose

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 gcc -o bcards src/main.c src/bcdeck.c src/bcutil.c docs/help.c
 cd decks
 pwd > decksavelocation
-mkdir ~/.config/bashcards
-mv decksavelocation ~/.config/bashcards/decksavelocation
+mkdir -v ~/.config/bashcards
+mv -v decksavelocation ~/.config/bashcards/decksavelocation
 cd ..
-gzip -k docs/bcards.1
-sudo cp docs/bcards.1.gz /usr/share/man/man1/bcards.1.gz
-
-sudo mv bcards /usr/bin/
+gzip -v -k docs/bcards.1
+sudo cp -v docs/bcards.1.gz /usr/share/man/man1/bcards.1.gz
+sudo mv -v bcards /usr/bin/
+echo "done!"
